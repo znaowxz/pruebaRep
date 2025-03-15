f9ad3d751dcfd76b428f05dc044db24dbb533a5a
znaowxz <anacoin12@gmail.com>
2025-03-15 20:41:48 +0100

    Conflict provoked

--- pruebaRep/src/main/java/org/iis2024/Date.java
+++ pruebaRep/src/main/java/org/iis2024/Date.java
@@ -59,10 +59,10 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
   }
 
   /**
