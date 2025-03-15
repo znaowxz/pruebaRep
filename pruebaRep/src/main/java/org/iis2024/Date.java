8139bc522b18f74c89a54482437f4fe4b8453be8
znaowxz <anacoin12@gmail.com>
2025-03-15 20:25:06 +0100

    Method added by Owner.

--- pruebaRep/src/main/java/org/iis2024/Date.java
+++ pruebaRep/src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+  public void methodAddedByOwner(){
+    System.out.println("Method added by the owner");
+
+  }
   /**
    * Retrieves the day component of this date.
    *
